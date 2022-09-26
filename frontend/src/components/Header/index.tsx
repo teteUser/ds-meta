493ea937cf0edc0799905b1a77d29b4388c5c37d
Tennison Capra <tennison_capra@hotmail.com>
2022-09-25 21:06:19 -0300

    other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import logo from '../../assets/img/logo.svg'
+import './style.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="imagem de logo"/>
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por Tennison Capra</p>
+            </div>
+        </header>
+    )
+  }
+  
+  export default Header
---
+++ frontend/src/components/SalesCard/index.tsx
@@ -0,0 +1,77 @@
+import './style.css'
+import NotificationButton from '../NotificationButton'
+
+function SalesCard() {
+    return (
+        <div class="dsmeta-card">
+                    <h2 className="dsmeta-sales-title">Vendas</h2>
+                    <div>
+                        <div className="dsmeta-form-control-container">
+                            <input class="dsmeta-form-control" type="text" name="" id="" />
+                        </div>
+                        <div className="dsmeta-form-control-container">
+                            <input className="dsmeta-form-control" type="text" name="" id="" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <table className="dsmeta-sales-table">
+                            <thead>
+                                <tr>
+                                    <th className="show992">ID</th>
+                                    <th className="show576">Data</th>
+                                    <th>Vendedor</th>
+                                    <th className="show992">Visitas</th>
+                                    <th className="show992">Vendas</th>
+                                    <th>Total</th>
+                                    <th>Notificar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className="show992">#341</td>
+                                    <td className="show576">07/09/2022</td>
+                                    <td>Anakin</td>
+                                    <td className="show992">15</td>
+                                    <td className="show992">11</td>
+                                    <td>R$ 55300.00</td>
+                                    <td>
+                                        <div className="dsmeta-red-btn-container">
+                                            <NotificationButton />
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="show992">#341</td>
+                                    <td className="show576">07/09/2022</td>
+                                    <td>Anakin</td>
+                                    <td className="show992">15</td>
+                                    <td className="show992">11</td>
+                                    <td>R$ 55300.00</td>
+                                    <td>
+                                        <div className="dsmeta-red-btn-container">
+                                            <NotificationButton />
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className="show992">#341</td>
+                                    <td className="show576">07/09/2022</td>
+                                    <td>Anakin</td>
+                                    <td className="show992">15</td>
+                                    <td className="show992">11</td>
+                                    <td>R$ 55300.00</td>
+                                    <td>
+                                        <div className="dsmeta-red-btn-container">
+                                            <NotificationButton />
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+    )
+}
+
+export default SalesCard
