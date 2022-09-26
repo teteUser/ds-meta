9ba9fb319109beb72fdcc9e3fc0afb15096a8ed1
Tennison Capra <tennison_capra@hotmail.com>
2022-09-25 21:29:55 -0300

    implementing useState for DatePicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,31 +1,37 @@
-import './style.css'
-import NotificationButton from '../NotificationButton'
+import './style.css';
+import NotificationButton from '../NotificationButton';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 
 function SalesCard() {
+
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const max = new Date();
+    const [minDate, setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
+
     return (
         <div class="dsmeta-card">
                     <h2 className="dsmeta-sales-title">Vendas</h2>
                     <div>
                         <div className="dsmeta-form-control-container">
-                        <DatePicker
-                            selected={new Date()}
-                            onChange={(date: Date) => {}}
-                            className="dsmeta-form-control"
-                            dateFormat="dd/MM/yyyy"
-                        />
+                            <DatePicker
+                                selected={minDate}
+                                onChange={(date: Date) => setMinDate(date)}
+                                className="dsmeta-form-control"
+                                dateFormat="dd/MM/yyyy"
+                            />
                         </div>
                         <div className="dsmeta-form-control-container">
-                        <DatePicker
-                            selected={new Date()}
-                            onChange={(date: Date) => {}}
-                            className="dsmeta-form-control"
-                            dateFormat="dd/MM/yyyy"
-                        />
+                            <DatePicker
+                                selected={maxDate}
+                                onChange={(date: Date) => setMaxDate(date)}
+                                className="dsmeta-form-control"
+                                dateFormat="dd/MM/yyyy"
+                            />
                         </div>
                     </div>
-
                     <div>
                         <table className="dsmeta-sales-table">
                             <thead>
@@ -86,4 +92,4 @@ function SalesCard() {
     )
 }
 
-export default SalesCard
+export default SalesCard;
