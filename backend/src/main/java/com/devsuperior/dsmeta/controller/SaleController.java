6fa419ee324ae0bfff70311c582e4a8b8c4d2426
Tennison Capra <tennison_capra@hotmail.com>
2022-09-27 13:14:07 -0300

    Heroku deploy

--- backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -7,8 +7,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping(value = "/sales")
 public class SaleController {
